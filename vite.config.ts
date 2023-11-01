85f23df38cc6d973137c5d37219d68295bf35bd4
Sydney <sydney.fryer@ai.metagon.com>
2023-11-01 17:52:28 +0200

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite'
-import handlebars from 'vite-plugin-handlebars';
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import handlebars from "vite-plugin-handlebars";
 
-const root = resolve(__dirname, 'src');
-const outDir = resolve(__dirname, 'dist');
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
 export default defineConfig({
   root,
   plugins: [
     handlebars({
-      partialDirectory: resolve(__dirname,'src', 'partials'),
+      partialDirectory: resolve(__dirname, "src", "partials"),
     }) as any,
   ],
   build: {
@@ -16,11 +16,12 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(root, 'index.html'),
-        contactUs: resolve(root, 'contact-us', 'index.html'),
-        emailDisclaimers: resolve(root, 'email-disclaimers', 'index.html'),
-        termsOfService: resolve(root, 'terms-of-service', 'index.html')
-      }
-    }
-  }
-})
+        main: resolve(root, "index.html"),
+        contactUs: resolve(root, "contact-us", "index.html"),
+        altair: resolve(root, "altair", "index.html"),
+        emailDisclaimers: resolve(root, "email-disclaimers", "index.html"),
+        termsOfService: resolve(root, "terms-of-service", "index.html"),
+      },
+    },
+  },
+});
