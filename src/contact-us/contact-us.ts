f8a9d16cb04ab01c761d438c6eaa2c65430f250c
Sydney <sydney.fryer@ai.metagon.com>
2023-12-05 08:22:01 +0200

    ...

--- src/contact-us/contact-us.ts
+++ src/contact-us/contact-us.ts
@@ -122,7 +122,7 @@ export class ContactUs {
                         'Content-Type': "application/json"
                       },
                     success: function(data) {
-                        //blah - remove this
+                        //blah - remove this -
                         console.log(data)
                         // If the API call is successful, display the dialog box
                         if(modal!==null)
